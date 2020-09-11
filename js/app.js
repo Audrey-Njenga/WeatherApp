c93697665ed5317f0cbf32d80cba2cf3c30265b4
Audrey-Njenga <a.njenga@alustudent.com>
2020-09-11 16:59:37 +0300

    Add cache, localstorage, PWA functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,137 @@
-var city = document.getElementById('city');
-var searchButton = document.getElementById('searchicon');
-var pinButton = document.getElementById('mapicon');
-var cityTemp = document.getElementById('cityTemp');
-var cityName = document.getElementById('cityName');
-var cityDesc = document.getElementById('cityDesc');
-var cityWeather = document.getElementById('cityWeather');
-var sunrise = document.getElementById('sunrise');
-var sunset = document.getElementById('sunset');
-var resultsDiv = document.getElementById('results');
+// registers service worker on window load
+if ('serviceWorker' in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register('../service.js')
+            .then(reg => console.log("Registered"))
+            .catch(err => console.log(`Error: ${err}`))
+    })
+}
+
+
+// declare variables for use
+let city = document.getElementById('city');
+let searchButton = document.getElementById('searchicon');
+let pinButton = document.getElementById('mapicon');
+let cityTemp = document.getElementById('cityTemp');
+let cityName = document.getElementById('cityName');
+let cityDesc = document.getElementById('cityDesc');
+let cityWeather = document.getElementById('cityWeather');
+let sunrise = document.getElementById('sunrise');
+let sunset = document.getElementById('sunset');
+let recent = document.getElementById('recent');
+let resultsDiv = document.getElementById('results');
+let errorBlock = document.getElementById('errorblock');
+let lastestSearch = Object.keys(localStorage)[0];
+let cities = [];
+cityName = lastestSearch.city;
+cityTemp = lastestSearch.temp;
+// main functionality
+// returns waether info on search
+searchButton.addEventListener('click', function () {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=' + city.value + '&units=metric&appid=74f71f68c5d571f7efd42959517e6e27')
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+
+            let name = data["name"];
+            let temp = data["main"]["temp"];
+            let desc = data["weather"][0]["main"];
+            let rise = data["sys"]["sunrise"];
+            let set = data["sys"]["sunset"];
+            rise = formatTime(rise);
+            set = formatTime(set);
+            temp = parseFloat(temp.toFixed(1)) + "&#8451;";
+            let statusCode = data["weather"][0]["id"];
+
+
+            cityWeather.setAttribute("data-feather", geticon(statusCode))
+            cityName.innerHTML = name;
+            cityDesc.innerHTML = desc;
+            cityTemp.innerHTML = temp;
+            city.value = "";
+            sunrise.innerHTML = rise;
+            sunset.innerHTML = set;
+            let obj = { city: name, temp: temp };
+            localStorage.setItem(name, JSON.stringify(obj));
+
+
+            document.getElementById("cityName").classList.add("popleft");
+            document.getElementById("cityTemp").classList.add("popleft");
+            document.getElementById("cityWeather").classList.add("popleft");
+            feather.replace();
+
+
+
+        }).catch(err => errorBlock.innerHTML = "City does not exist. Try again");
+
+
+})
+
+// returns previous searches from localstorage
+function previousSearches() {
+
+
+    if (localStorage.length == 0) {
+        previous.innerHTML = "No recent searches";
+    } else {
+        Object.keys(localStorage).forEach((cty) => {
+            cacheCity = JSON.parse(localStorage.getItem(cty));
+            let ctyName = cacheCity.city;
+            let ctyTemp = cacheCity.temp;
+            recent.insertAdjacentHTML("afterend", `<br><p class="cities">${ctyName} : ${ctyTemp}</p>`);
+       }        
+    )}
+}
+
+
+function deleteSearch() {
+
+}
+function updateRecent(newCity) {
+    let recents = JSON.parse(localStorage.getItem("arr"));
+    recents = cachedSearches.push(newCity);
+    // recentSearches  = recentSearches.add(recents);
+    let updated = JSON.stringify(recents);
+    localStorage.setItem("arr", updated);
+}
+
+
+// time formatter
+
+function formatTime(time) {
+    let date = new Date(time * 1000);
+    let finalTime = date.toLocaleTimeString(undefined, {
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+    return finalTime;
+}
+
+
+
+// returns an icon based on the weather code
+function geticon(weathercode) {
+    switch (true) {
+        case (weathercode >= 200) & (weathercode < 300):
+            return 'cloud-lightning';
+        case (weathercode >= 300) & (weathercode < 400):
+            return 'cloud-drizzle';
+        case (weathercode >= 500) & (weathercode < 600):
+            return 'cloud-rain';
+        case (weathercode >= 600) & (weathercode < 700):
+            return 'cloud-snow';
+        case (weathercode >= 801) & (weathercode < 900):
+            return 'sun';
+        default:
+            return 'wind';
+
+    }
+
+}
+
+previousSearches();
+
+//To be implemented
 // pick location from user
 // pinButton.addEventListener('click', function () {
 //     let longitude;
@@ -23,35 +147,3 @@ var resultsDiv = document.getElementById('results');
 //     }
 
 // })
-
-searchButton.addEventListener('click', function () {
-    fetch('//api.openweathermap.org/data/2.5/weather?q=' + city.value + '&units=metric&appid=74f71f68c5d571f7efd42959517e6e27')
-        .then((response) => {
-            return response.json()
-        }).then((data) => {
-
-            var name = data["name"];
-            var temp = data["main"]["temp"];
-            var desc = data["weather"][0]["main"];
-            var rise = data["sys"]["sunrise"];            
-            var set = data["sys"]["sunset"];
-            rise = new Date(rise).toLocaleTimeString('en-US');
-            set = new Date(set).toLocaleTimeString('en-US');
-            temp = temp + "&#8451;";
-
-            cityName.innerHTML = name;
-            cityDesc.innerHTML = desc;
-            cityTemp.innerHTML = temp;
-            city.value = "";
-            sunrise.innerHTML = rise;
-            sunset.innerHTML = set;
-
-            document.getElementById("cityName").classList.add("popleft");
-            document.getElementById("cityTemp").classList.add("popleft");
-            document.getElementById("cityWeather").classList.add("popleft");
-
-
-        }).catch(err => alert("City does not exist"))
-
-
-})
